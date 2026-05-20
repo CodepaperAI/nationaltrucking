a66ebfd98ad732b1ee4a365f89c1f7dd884f4a63
jagat-developer <jagat@codepaper.com>
2026-05-19 22:29:38 -0400

    Disable Vercel image optimization

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
